53dcdd84e83507b65a0fdff8d4fc891d68d0b5d8
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-01-13 17:47:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,13 +30,13 @@ function sayHiToGrandma(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string === string.toLowerCase){
+  if (string.toLowerCase){
   return ('I can\'t hear you!')}
   
-    else if (string === string.toUpperCase){
+    else if (string.toUpperCase){
       return ('YES INDEED!')}
       
-        else if (string === 'I love you, Grandma.'){
+        else if ('I love you, Grandma.'){
         return ('I love you, too.') }
       
 
